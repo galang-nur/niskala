c2c358d393ae17f08f8b7cb1c6d92124239eef16
Galang <penggalang.nur@gmail.com>
2024-03-09 16:44:35 +0700

    Published All Files
    Publishing all related files to the github repository.

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+function heroScroll() {
+    window.scrollTo({left:0, 
+        top:800, 
+        behavior: "smooth"
+    });  
+};
+
+function backToTopScroll() {
+    window.scrollTo({
+        left:0,
+        top:0,
+        behavior: "smooth"
+    });
+};
+
+function practiceAreaScroll() {
+    window.scrollTo({
+        left:0,
+        top:1200,
+        behavior: "smooth"
+    });
+};
+
+function AdvocateScroll() {
+    window.scrollTo({
+        left:0,
+        top:1800,
+        behavior: "smooth"
+    });
+};
+
+function aboutUsScroll() {
+    window.scrollTo({
+        left:0,
+        top:4000,
+        behavior: "smooth"
+    });
+};
